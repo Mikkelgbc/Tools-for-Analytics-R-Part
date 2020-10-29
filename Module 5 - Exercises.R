dab49e02d4e89bac7d108a886a091f377fab455a
Mikkelgbc <Mikkelgbc@gmail.com>
2020-10-29 12:47:02 +0100

    Commit

--- Module 5 - Exercises.R
+++ Module 5 - Exercises.R
@@ -13,6 +13,7 @@ sum_n <- function(n){
 
 sum_n(5000)
 
+
 # Task 2: Write a function compute_s_n that for any given n computes the sum
 compute_s_n <- function(n){
   return(sum((1:n)^2))
@@ -20,12 +21,14 @@ compute_s_n <- function(n){
 
 compute_s_n(10)
 
+
 # Task 3: Define an empty numerical vector s_n of size 25 and store in the results using a for-loop. Confirm that the formula for the sum is
 s_n <- vector("numeric",25)
 
 for(n in 1:25){
   s_n[n] <- compute_s_n(n)
 }
+
 s_n
 
 compute_s_n_alt <- function(n){
@@ -41,11 +44,15 @@ for(n in 1:25){
 
 compute_s_n_alt(n)
 
+
 # Task 4: Write a function biggest which takes two integers as arguments. Let the function return 1 if the first argument is larger than the second and return 0 otherwise.
 biggest <- function(a, b){
   if(a > b) return(1)
   return(0)
 }
+
+biggest(5,2)
+
 
 # Task 5: Write a function that returns the shipping cost as 10% of the total cost of an order (input argument).
 ShippingCost <- function(total){
@@ -53,6 +60,7 @@ ShippingCost <- function(total){
 }
 
 ShippingCost(450)
+
 
 # Task 6: Given Question 5, rewrite the function so the percentage is an input argument with a default of 10%
 ShippingCost <- function(total,pct=0.1){
@@ -63,6 +71,7 @@ ShippingCost(450)
 
 ShippingCost(450,0.2)
 
+
 # Task 7: Given Question 5, the shipping cost can be split into parts. One part is gasoline which is 50% of the shipping cost. 
 # Write a function that has total cost as input argument and calculate the gasoline cost and use the function defined in Question 5 inside it
 
@@ -72,15 +81,17 @@ GasolineCost <- function(total){
 
 GasolineCost(450)
 
+
 # Task 8: Given Question 6, the shipping cost can be split into parts. One part is gasoline which is 50% of the shipping cost. 
 # Write a function that has total cost a input argument and calculate the gasoline cost and use the function defined in Question 6 inside it. 
 # Hint: Use the ... argument to pass arguments to shipping_cost.
 
 GasolineCost <- function(total,...){
-  return(0.5*ShippingCost(total))
+  return(0.5*ShippingCost(total,...))
 }
 
 GasolineCost(450)
+
 
 # Task 9: Given Question 8, write a function costs that, given total cost, returns the total cost, shipping cost and gasoline cost.
 costs <- function(total,...){
@@ -88,7 +99,7 @@ costs <- function(total,...){
   return(lst)
 }
 
-costs(450)
+costs(450, pct = 0.15)
 
 
 # 5.8.2 Exercise (scope)
@@ -105,6 +116,7 @@ my_func(7)
 
 # Answer: The value of x is 3, as the "x <- 5" is contained within the function (Local variable)
 
+
 # Task 2: Is there any problems with the following code?
 x <- 3
 
@@ -116,6 +128,7 @@ my_func(7)
 
 # Answer: The code runs. But it is not good coding practice to call global variables inside a function (x). Instead x should have been an argument to the function.
 
+
 # Task 3: Have a look at the documentation for operator <<- (run ?'<--'). After running the code below, what is the value of variable x?
 x <- 3
 
@@ -126,6 +139,7 @@ my_func <- function(y){
 }
 
 # Answer: That value is still 3 since my_func has not been called yet.
+
 
 # Task 4: After running the code below, what is the value of variable x and output of the function call?
 x <- 3
@@ -175,28 +189,28 @@ setup_costs
 # Step 2: Set next job in Step 1 to current job and go to Step 1 if not all jobs are done.
 
 greedy <- function(startup, setup) {
-  jobs <- nrow(setup)
-  cur_job <- which.min(startup)
-  cost <- startup[cur_job]
+  jobs <- nrow(setup) # Defines the number of jobs
+  cur_job <- which.min(startup) # Defines current job as the job with the lowest startup cost
+  cost <- startup[cur_job] # Cost is equal to the startup cost of the current job
   
   # cat("Start job:", cur_job, "\n")
-  job_seq <- cur_job
-  setup[, cur_job] <- NA
+  job_seq <- cur_job # Sets job sequence equal to the current job
+  setup[, cur_job] <- NA # We set the columns of the current jobs startup costs to NA to exclude it from being picked again.
   
-  for (i in 1:(jobs-1)) {
+  for (i in 1:(jobs-1)) { # Loops trough the remaining jobs and set the next job equal to job with the lowest startup cost.
     next_job <- which.min(setup[cur_job, ])
     
-    # cat("Next job:", next_job, "\n") 
-    cost <- cost + setup[cur_job, next_job]
-    job_seq <- c(job_seq, next_job)
-    cur_job <- next_job
-    setup[, cur_job] <- NA
+    # cat("Next job:", next_job, "\n")
+    cost <- cost + setup[cur_job, next_job] # Updates costs during loop
+    job_seq <- c(job_seq, next_job) # Notes the job sequence
+    cur_job <- next_job # Sets the current job equal to the next job
+    setup[, cur_job] <- NA # Set the columns of the current jobs startup costs to NA to exclude it from being picked again.
   }
   # print(setup)
-  return(list(seq = job_seq, cost = cost))
+  return(list(seq = job_seq, cost = cost)) # Returns a list of the job sequence and cost
 }
 
-greedy(startup_costs, setup_costs)
+greedy(startup_costs, setup_costs) # Returns the result of the function
 
 # First, the job with minimum startup cost is found using function which.min and we define cost as the startup cost. 
 # We use cat to make some debugging statements and initialize job_seq with the first job. 
@@ -209,16 +223,17 @@ greedy(startup_costs, setup_costs)
 # Step 0: Subtract minimum of startup and setup cost for each job from setup and startup costs (that is columnwise)
 # Step 1: Call the greedy algorithm with the modified costs. Note that the total cost returned has to be modified a bit.
 
-# Task: Implement a better function calculating a better strategy. Hint: to find the minimum column costs, you may use apply(rbind(startup, setup), 2, min, na.rm = T).
+# Task: Implement a better function calculating a better strategy. 
+# Hint: to find the minimum column costs, you may use apply(rbind(startup, setup), 2, min, na.rm = T).
 better <- function(startup,setup){
-  jobs <- nrow(setup)
-  min_col_val <- apply(rbind (startup, setup), 2, min, na.rm = TRUE)
-  startup <- startup - min_col_val
-  min_mat <- matrix(rep(min_col_val, jobs), ncol=jobs, byrow=TRUE)
-  setup <- setup - min_mat
-  lst <- greedy(startup, setup)
-  lst$cost <- lst$cost + sum(min_col_val)
-  return(lst)
+  jobs <- nrow(setup) # Defines the number of jobs
+  min_col_val <- apply(rbind (startup, setup), 2, min, na.rm = TRUE) # Returns a new vector with the minimum column values
+  startup <- startup - min_col_val # Subtracts the minimum column value from the startup costs matrix
+  min_mat <- matrix(rep(min_col_val, jobs), ncol = jobs, byrow = TRUE) # Creates a matrix of the minimum values by size jobs x jobs (5 x 5)
+  setup <- setup - min_mat # Subtracts the minimum matrix from the setup costs
+  lst <- greedy(startup, setup) # Call the greedy function with the modified setup costs
+  lst$cost <- lst$cost + sum(min_col_val) # We add the values back to the costs
+  return(lst) # Returns list
 }
 
 better(startup_costs, setup_costs)
